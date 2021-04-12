35a520832909ae0fabeb231989343b7153e34ab6
aycan <aycanyerdelen@gmail.com>
2021-04-12 13:59:51 +0300

    files updated

---
+++ CountdownTimer-JS/app.js
@@ -0,0 +1,15 @@
+const start = document.getElementById("start");
+const reset = document.getElementById("reset");
+const stop = document.getElementById("stop");
+
+const hour = document.getElementById("hour");
+const min = document.getElementById("minute");
+const sec = document.getElementById("sec");
+
+start.addEventListener("click", () => {
+  console.log("click");
+});
+
+reset.addEventListener("click", () => {
+  console.log("clicked");
+});
